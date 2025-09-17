5f36a050b8e42fc00899226628be8d12aec6ec5f
Samaresh Bhowmik <samareshbhowmik@yahoo.com>
2025-09-17 17:20:05 +0100

    Initial commit for Sunnyside_Day_Care project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Sunnyside_Day_Care.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
